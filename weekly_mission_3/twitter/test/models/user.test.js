4f154903cc509a72718fc2e5aca3148eb6e9c3e2
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-04-21 16:11:08 -0500

    Requerimiento 3

--- weekly_mission_3/twitter/test/models/user.test.js
+++ weekly_mission_3/twitter/test/models/user.test.js
@@ -14,5 +14,12 @@ describe("Unit Tests for User class", () => {
     expect(user.dateCreated).not.toBeUndefined(); // verifca que el valor no sea undefine
     expect(user.lastUpdated).not.toBeUndefined();
   })
-
+  
+  test("Add getters", () => {
+    const user = new User(1, "carlogilmar", "Carlo", "Bio");
+    expect(user.getUsername).toBe("carlogilmar");
+    expect(user.getBio).toBe("Bio");
+    expect(user.getDateCreated).not.toBeUndefined();
+    expect(user.getLastUpdated).not.toBeUndefined();
+  })
 })
