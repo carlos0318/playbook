c60b6289deafb532e4074851c0c9d7657557aed1
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-04-21 19:02:33 -0500

    Sprint 3: Requerimiento 4

--- weekly_mission_3/twitter/app/views/UserView.js
+++ weekly_mission_3/twitter/app/views/UserView.js
@@ -1,6 +1,14 @@
+const UserService = require('./../services/UserService');
+
 class UserView {
   static createUser(payload) {
+    const keys = ['username', 'id', 'name'];
+
     if (!payload) return {error: 'payload no existe'};
+    if (Object.values(payload).some(value => !value)) return {error: 'necesitan tener un valor válido'};
+    if (keys.some(key => !payload[key])) return {error: 'necesitan tener un valor válido'};
+  
+    return UserService.create(payload.id, payload.username, payload.name);
   }
 }
 
